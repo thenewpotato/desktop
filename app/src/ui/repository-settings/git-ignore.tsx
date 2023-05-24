3535738035b90e7954b1383a53843331ab39538d
Tiger Wang <tiger.wangj@gmail.com>
2023-05-24 16:36:55 -0500

    gitignore improvement
    - add hint text for empty gitignore

--- app/src/ui/repository-settings/git-ignore.tsx
+++ app/src/ui/repository-settings/git-ignore.tsx
@@ -3,6 +3,10 @@ import { DialogContent } from '../dialog'
 import { TextArea } from '../lib/text-area'
 import { LinkButton } from '../lib/link-button'
 import { Ref } from '../lib/ref'
+import { PopoverDropdown } from "../lib/popover-dropdown";
+import { Row } from "../lib/row";
+import { getGitIgnoreNames, getGitIgnoreText } from '../add-repository/gitignores'
+
 
 interface IGitIgnoreProps {
   readonly text: string | null
@@ -10,8 +14,25 @@ interface IGitIgnoreProps {
   readonly onShowExamples: () => void
 }
 
+interface IGitIgnoreState {
+  /** The names of the available gitignore templates. */
+  readonly gitIgnoreNames: ReadonlyArray<string> | null
+}
+
 /** A view for creating or modifying the repository's gitignore file */
-export class GitIgnore extends React.Component<IGitIgnoreProps, {}> {
+export class GitIgnore extends React.Component<IGitIgnoreProps, IGitIgnoreState> {
+  public async componentDidMount() {
+    const gitIgnoreNames = await getGitIgnoreNames()
+
+    this.setState({ gitIgnoreNames })
+  }
+
+  private renderGitIgnoreList() {
+    return this.state.gitIgnoreNames.map(name => (
+      <Row key={name}>{name}</Row>
+    ))
+  }
+
   public render() {
     return (
       <DialogContent>
@@ -22,6 +43,14 @@ export class GitIgnore extends React.Component<IGitIgnoreProps, {}> {
           <LinkButton onClick={this.props.onShowExamples}>
             Learn more about gitignore files
           </LinkButton>
+        </p>
+
+        <p>
+          It looks like <Ref>.gitignore</Ref> is empty. You can start from scratch or{' '}
+          <PopoverDropdown contentTitle={"Choose a .gitignore template"} buttonContent={"select a template"} label={''}>
+            {this.renderGitIgnoreList()}
+          </PopoverDropdown>
+          {' '}to get started.
         </p>
 
         <TextArea
